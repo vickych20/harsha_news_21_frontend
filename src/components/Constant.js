9d64b4db9ddb59ed8a9e7af38693a9b9b28d43f1
dauqu <sujeetsingh734321@gmail.com>
2023-01-21 17:28:06 +0530

    added new bakend link

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,2 +1,4 @@
-export const API = "https://news.dauqu.host/api/v1";
+// export const API = "https://news.dauqu.host/api/v1";
+export const API = "https://harshanews21backend-production.up.railway.app//api/v1";
+
 // export const API = "http://localhost:4000/api/v1";
